c8d19adf612b50aa560ca2016c790d7c76308be8
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-04 21:50:14 +0500

    /gods cmd improvement

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-local gods = core.get_mod_storage()
+local s = core.get_mod_storage()
 core.register_privilege("godmode","Allows to make players gods")
 core.register_on_joinplayer(function(player)
     local name = player:get_player_name()
     if not name then return end
-    local isgod = gods:get_string(name)
+    local isgod = s:get_string(name)
     if isgod == "1" then
         core.after(0.1,function()
             player:set_armor_groups({immortal=1})
@@ -17,14 +17,14 @@ core.register_chatcommand("god",{
     func = function(name,param)
         if param == "" then param = name end
         local player = core.get_player_by_name(param)
-        local isgod = gods:get_string(param)
+        local isgod = s:get_string(param)
         if isgod == "1" then
             if player then player:set_armor_groups({immortal=0}) end
-            gods:set_string(param,"")
+            s:set_string(param,"")
             return true, "Godmode disabled for "..param
         else
             if player then player:set_armor_groups({immortal=1}) end
-            gods:set_string(param,"1")
+            s:set_string(param,"1")
             return true, "Godmode enabled for "..param
         end
 end})
@@ -32,10 +32,10 @@ core.register_chatcommand("gods",{
     description = "Show list of godmoded players",
     privs = {godmode=true},
     func = function(name,param)
-        local msg = "Gods: "
-        local tabl = gods:to_table().fields
+        local out = {}
+        local tabl = s:to_table().fields
         for nick,val in pairs(tabl) do
-            msg = msg..nick..", "
+            table.insert(out,nick)
         end
-        return true, msg:sub(1,-3)
+        return true, "Gods: "..table.concat(out,", ")
 end})
